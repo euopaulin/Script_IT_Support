df2b753904e8a8a8888c85c6b039b5c07519fa0b
Paulo Henrique <paulohspfc209@gmail.com>
2025-09-01 16:31:21 -0300

    Update

--- script.bat
+++ script.bat
@@ -1,13 +1,15 @@
+color 5F
 @echo off
 goto menu
 
 :menu
 cls
+chcp 65001
 echo ----------------------------------------------
 echo            Menu Suporte N2 - IPEA
-echo  Melhores comandos para manuntecao de maquinas
-echo ----------------------------------------------
+echo Comandos Essenciais de Manutenção para Windows 10 e 11
 echo           Feito por Paulo Henrique
+echo ----------------------------------------------
 echo              Escolha uma opcao:
 echo ----------------------------------------------
 echo 1. Rodar SFC/SCANNOW
@@ -36,12 +38,14 @@ if "%escolha%"=="8" goto programa8
 if "%escolha%"=="9" goto programa9
 if "%escolha%"=="10" goto sair
 
-echo Opcao invalida. Por favor, digite um numero de 1 a 10.
+chcp 65001
+echo Opção invalida. Por favor, digite um número de 1 a 10.
 pause
 goto menu
 
 :sfc
-echo Opcao 1 selecionada: Rodando SFC/SCANNOW...
+chcp 65001
+echo Opção 1 selecionada: Rodando SFC/SCANNOW...
 sfc /scannow
 if %errorlevel% neq 0 (
     echo Erro ao executar SFC/SCANNOW. Execute o arquivo como Administrador.
@@ -52,7 +56,8 @@ pause
 goto menu
 
 :chkdsk
-echo Opcao 2 selecionada: Rodando CHKDSK...
+chcp 65001
+echo Opção 2 selecionada: Rodando CHKDSK...
 set /p "drive=Digite a letra do drive (ex: C)? "
 echo Verificando o drive %drive%...
 chkdsk %drive%: /f /r
@@ -65,7 +70,8 @@ pause
 goto menu
 
 :programa3
-echo Opcao 3 selecionada: Executando programa Driver Booster...
+chcp 65001
+echo Opção 3 selecionada: Executando programa Driver Booster...
 start "" "\\storage1\bibsoft\# PH - Softwares\Dia a dia\driver_booster_setup.exe"
 if %errorlevel% neq 0 (
     echo Erro ao executar o programa.
@@ -76,7 +82,8 @@ pause
 goto menu
 
 :programa4
-echo Opcao 4 selecionada: Executando programa Revo Uninstaller...
+chcp 65001
+echo Opção 4 selecionada: Executando programa Revo Uninstaller...
 start "" "\\storage1\bibsoft\# PH - Softwares\Dia a dia\RevoUninstaller_Portable\RevoUPort.exe
 if %errorlevel% neq 0 (
     echo Erro ao executar o programa.
@@ -87,7 +94,8 @@ pause
 goto menu
 
 :programa5
-echo Opcao 5 selecionada: Executando Windows Defender...
+chcp 65001
+echo Opção 5 selecionada: Executando Windows Defender...
 start "" "\\storage1\bibsoft\#BibSoft_NEW#\1_ SOFTWARES PADRÃO\PADRÕES\WindowsDefenderATPLocalOnboardingScript.cmd"
 if %errorlevel% neq 0 (
     echo Erro ao executar o programa.
@@ -98,7 +106,8 @@ pause
 goto menu]
 
 :programa6
-echo Opcao 6 selecionada: Executando comando para limpar arquivos temporarios...
+chcp 65001
+echo Opção 6 selecionada: Executando comando para limpar arquivos temporarios...
 del /F /Q /S "%TEMP%\*"
 for /D %%p in ("%TEMP%\*") do rmdir "%%p" /S /Q
 if %errorlevel% neq 0 (
@@ -110,7 +119,8 @@ pause
 goto menu
 
 :programa7
-echo Opcao 7 selecionada: Rodando restauração de imagem do sistema...
+chcp 65001
+echo Opção 7 selecionada: Rodando restauração de imagem do sistema...
 sfc DISM /Online /Cleanup-Image /RestoreHealth
 if %errorlevel% neq 0 (
     echo Erro ao executar o programa.
@@ -121,7 +131,8 @@ pause
 goto menu
 
 :programa8
-echo Opcao 8 selecionada: Verificando o IP da maquina...
+chcp 65001
+echo Opção 8 selecionada: Verificando o IP da maquina...
 ipconfig
 if %errorlevel% neq 0 (
     echo Erro ao executar o programa.
@@ -132,7 +143,8 @@ pause
 goto menu
 
 :programa9
-echo Opcao 9 selecionada: Pingar um endereco IP...
+chcp 65001
+echo Opção 9 selecionada: Pingar um endereco IP...
 set /p "endereco=Digite o endereco IP ou HostName para pingar: "
 ping %endereco%
 if %errorlevel% neq 0 (
